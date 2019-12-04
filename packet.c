15080efe8d54a13fc08dfc45901cffea7cd7568e
Kyle Patterson <kdpatters@users.noreply.github.com>
2019-12-03 23:47:32 -0500

    Added packet.h and packet.c to repository tracking

---
+++ packet.c
@@ -0,0 +1,58 @@
+/*
+ * packet.c
+ */
+
+#include <string.h>
+#include <stdlib.h>
+#include "packet.h"
+#include "bt_parse.h"
+
+/*
+ * init_packet
+ */
+void init_packet(data_packet_t *pack, char pack_type, int seq_num, int ack_num,
+    char *payload, int payload_len) {
+
+    // Initialize packet
+    memset(pack, 0, sizeof(*pack));
+
+    // Calculate the packet length
+    short packet_len = sizeof(header_t) + payload_len;
+    if (packet_len > PACKETLEN) {
+        perror("Something went wrong: constructed packet is longer than the \
+maximum possible length.");
+        exit(1);
+    }
+    else if (packet_len <= 0) {
+        perror("Something went wrong: constructed packet has a \
+non-positive length.");
+        exit(1); 
+    }
+
+    // Create the header
+    header_t *header = &pack->header;
+    header->magicnum = MAGICNUM;
+    header->version = VERSIONNUM;
+    header->packet_type = pack_type;
+    header->header_len = sizeof(header);
+    header->packet_len = packet_len;
+    header->seq_num = seq_num;
+    header->ack_num = ack_num;
+
+    // Copy the data into the packet
+    memcpy(pack->data, payload, payload_len);
+}
+
+void send_pack(data_packet_t *pack, struct sockaddr_in *dest, bt_config_t *config) {
+    // Create the socket
+    int sockfd = config->sock;
+
+    struct sockaddr_in myaddr;
+    memset(&myaddr, 0, sizeof(myaddr));
+    memcpy(&myaddr.sin_addr, &dest->sin_addr, sizeof(dest->sin_addr));
+    memcpy(&myaddr.sin_port, &dest->sin_port, sizeof(dest->sin_port));
+    myaddr.sin_family = AF_INET;
+
+    sendto(sockfd, pack, pack->header.packet_len, 0, 
+    (const struct sockaddr *) &myaddr, sizeof(myaddr));
+}
---
+++ packet.h
@@ -0,0 +1,45 @@
+/*
+ * packet.h
+ * 
+ * Header file for things about packets.
+ */
+
+#include "chunk.h"
+
+// Header constants
+#define MAGICNUM 3752
+#define VERSIONNUM 1
+
+// Packet types
+#define WHOHAS_TYPE 0
+#define IHAVE_TYPE 1
+#define GET_TYPE 2
+#define DATA_TYPE 3
+#define ACK_TYPE 4
+#define DENIED_TYPE 5
+
+#define PACKETLEN 1500
+#define PADDING 3
+#define CHK_COUNT 1
+#define HEADER_SIZE sizeof(header_t)
+#define DATALEN (PACKETLEN - HEADER_SIZE)
+#define SPACELEFT (DATALEN - PADDING - CHK_COUNT)
+#define MAX_CHK_HASHES (SPACELEFT / CHK_HASH_BYTES)
+
+typedef struct header_s {
+  short magicnum;
+  char version;
+  char packet_type;
+  short header_len;
+  short packet_len; 
+  u_int seq_num;
+  u_int ack_num;
+} header_t;  
+
+typedef struct data_packet {
+  header_t header;
+  char data[DATALEN];
+} data_packet_t;
+
+void init_packet(data_packet_t *, char, int, int, char *, int);
+void send_pack(data_packet_t *, struct sockaddr_in *, bt_config_t *);
